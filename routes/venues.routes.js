dba8c914c8c7d4f08dc2a1fea0fb7424e9b363df
Rosu Mihai <malexandru.rosu@gmail.com>
2018-10-11 15:53:28 +0300

    removed unused comment

--- routes/venues.routes.js
+++ routes/venues.routes.js
@@ -6,7 +6,6 @@ module.exports = [
       path: '/api/venues',
       handler: function (request, reply) {
         Venues.find(function(error, venues) {
-            console.log('This is the right path brother');
               if (error) {
                   console.error(error);
               }
