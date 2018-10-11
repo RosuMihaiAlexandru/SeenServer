21bfdbf31eb28337f9508f95557cc7416e2a6786
Rosu Mihai <malexandru.rosu@gmail.com>
2018-10-11 12:27:41 +0300

    added message to path

--- routes/venues.routes.js
+++ routes/venues.routes.js
@@ -6,6 +6,7 @@ module.exports = [
       path: '/api/venues',
       handler: function (request, reply) {
         Venues.find(function(error, venues) {
+            console.log('This is the right path brother');
               if (error) {
                   console.error(error);
               }
