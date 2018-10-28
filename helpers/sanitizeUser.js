3670567b434cb8399b363aaeea6addf5c4411710
Florin G. Pantiru <fpantiru@ditech.ro>
2018-10-28 19:28:18 +0200

    added avatar for user

--- helpers/sanitizeUser.js
+++ helpers/sanitizeUser.js
@@ -4,6 +4,7 @@ const sanitizeUser = user => ({
     fullName: user.userName,
     email: user.email,
     myId: user._id,
+    profileImage: user.profileImage.media,
   });
   
   module.exports=sanitizeUser;
