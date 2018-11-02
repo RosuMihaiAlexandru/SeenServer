bee12ad83bffbb15d9709a102e2c4c4f8c662d00
Rosu Mihai <malexandru.rosu@gmail.com>
2018-11-03 00:36:59 +0200

    fixed bug

--- handlers/loadExpoPushToken.js
+++ handlers/loadExpoPushToken.js
@@ -5,12 +5,14 @@ module.exports = async function (request, reply) {
     var loggedInUserId = request.params.loggedInUserId.toString();
 
     await User.findOne({ _id: loggedInUserId }).then(user => {
-        if (user) {
+        if (user.expoPushToken !== undefined) {
             reply({
                 expoPushToken: user.expoPushToken
             }).code(200);
         } else {
-            reply(Boom.notFound("User not found")).code(404);
+            reply({
+                expoPushToken: 'NotFound'
+            }).code(404);
         }
     });
 };
