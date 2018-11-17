0761c7b8f4664e40f0d738e1cabcc30078577f5d
Rosu Mihai <malexandru.rosu@gmail.com>
2018-11-18 00:52:30 +0200

    added notifications

--- handlers/notifications/matchWithNotifications.js
+++ handlers/notifications/matchWithNotifications.js
@@ -5,28 +5,57 @@ const PushMessage = require("../../models/PushMessage")
 module.exports = async function (request, reply) {
     const match = await createOrUpdateMatch(request, reply);
     if (match.user1Liked && match.user2Liked) {
-        var member1ExpoPushTokens = request.payload.member1ExpoPushTokens;
-        var member2ExpoPushTokens = request.payload.member2ExpoPushTokens;
+        var member1PlayerIds = request.payload.member1PlayerIds;
+        var member2PlayerIds = request.payload.member2PlayerIds;
         var member1Name = request.payload.member1Name;
         var member2Name = request.payload.member2Name;
 
-        for (var i = 0, len = member1ExpoPushTokens.length; i < len; i++) {
-            NotificationsProcessor.process(new PushMessage({
-                to: member1ExpoPushTokens[i],
-                sound: 'default',
-                body: member2Name + ' likes you too!',
-                data: { withSome: 'data' },
-            }));
-          }
 
-          for (var i = 0, len = member2ExpoPushTokens.length; i < len; i++) {
-            NotificationsProcessor.process(new PushMessage({
-                to: member2ExpoPushTokens[i],
-                sound: 'default',
-                body: member1Name + ' likes you too!',
-                data: { withSome: 'data' },
-            }));
-          }
+        var sendNotification = function(data) {
+            var headers = {
+              "Content-Type": "application/json; charset=utf-8"
+            };
+            
+            var options = {
+              host: "onesignal.com",
+              port: 443,
+              path: "/api/v1/notifications",
+              method: "POST",
+              headers: headers
+            };
+            
+            var https = require('https');
+            var req = https.request(options, function(res) {  
+              res.on('data', function(data) {
+                console.log("Response:");
+                console.log(JSON.parse(data));
+              });
+            });
+            
+            req.on('error', function(e) {
+              console.log("ERROR:");
+              console.log(e);
+            });
+            
+            req.write(JSON.stringify(data));
+            req.end();
+          };
+          
+          var message1 = { 
+            app_id: "e8d3a93c-398c-407d-9219-8131322767a0",
+            contents: {"en": member2Name + ' likes you too!'},
+            include_player_ids: member1PlayerIds
+          };
+
+          var message2 = { 
+            app_id: "e8d3a93c-398c-407d-9219-8131322767a0",
+            contents: {"en": member1Name + ' likes you too!'},
+            include_player_ids: member2PlayerIds
+          };
+          
+          
+          sendNotification(message1);
+          sendNotification(message2);
     }
     reply(match);
 }
