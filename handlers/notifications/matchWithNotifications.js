44a1442a10a1b703ad2efdcf33b2009cb0f6ae53
Rosu Mihai <malexandru.rosu@gmail.com>
2018-11-18 01:21:43 +0200

    refactored server

--- handlers/notifications/matchWithNotifications.js
+++ handlers/notifications/matchWithNotifications.js
@@ -9,38 +9,7 @@ module.exports = async function (request, reply) {
         var member2PlayerIds = request.payload.member2PlayerIds;
         var member1Name = request.payload.member1Name;
         var member2Name = request.payload.member2Name;
-
-
-        var sendNotification = function(data) {
-            var headers = {
-              "Content-Type": "application/json; charset=utf-8"
-            };
-            
-            var options = {
-              host: "onesignal.com",
-              port: 443,
-              path: "/api/v1/notifications",
-              method: "POST",
-              headers: headers
-            };
-            
-            var https = require('https');
-            var req = https.request(options, function(res) {  
-              res.on('data', function(data) {
-                console.log("Response:");
-                console.log(JSON.parse(data));
-              });
-            });
-            
-            req.on('error', function(e) {
-              console.log("ERROR:");
-              console.log(e);
-            });
-            
-            req.write(JSON.stringify(data));
-            req.end();
-          };
-          
+         
           var message1 = { 
             app_id: "e8d3a93c-398c-407d-9219-8131322767a0",
             contents: {"en": member2Name + ' likes you too!'},
@@ -52,10 +21,9 @@ module.exports = async function (request, reply) {
             contents: {"en": member1Name + ' likes you too!'},
             include_player_ids: member2PlayerIds
           };
-          
-          
-          sendNotification(message1);
-          sendNotification(message2);
+          NotificationsProcessor.process(message1);
+          NotificationsProcessor.process(message2);
+
     }
     reply(match);
 }
--- notifications/NotificationsProcessor.js
+++ notifications/NotificationsProcessor.js
@@ -1,93 +1,38 @@
 const { Expo } =  require('expo-server-sdk');
 
 module.exports = {
-    pushNotificationsQueue: [],
-    hasStartedProcessingBatch: false,
-
-    enqueue(pushMessage) {
-        this.pushNotificationsQueue.push(pushMessage);
-    },
 
     async process(pushMessage) {
-        this.enqueue(pushMessage);
-        let self = this;
-        if (!self.hasStartedProcessingBatch) {
-            self.hasStartedProcessingBatch = true;
-            setTimeout(async function () {
-                 await self.sendNotification(self.pushNotificationsQueue);
-                 self.hasStartedProcessingBatch = false;
-                 self.pushNotificationsQueue = [];
-            }, 1000);
-        }
-    },
+        var sendNotification = function(data) {
+            var headers = {
+              "Content-Type": "application/json; charset=utf-8"
+            };
+            
+            var options = {
+              host: "onesignal.com",
+              port: 443,
+              path: "/api/v1/notifications",
+              method: "POST",
+              headers: headers
+            };
+            
+            var https = require('https');
+            var req = https.request(options, function(res) {  
+              res.on('data', function(data) {
+                console.log("Response:");
+                console.log(JSON.parse(data));
+              });
+            });
+            
+            req.on('error', function(e) {
+              console.log("ERROR:");
+              console.log(e);
+            });
+            
+            req.write(JSON.stringify(data));
+            req.end();
+          };
 
-    async sendNotification(pushNotificationsQueue) {
-        // Create a new Expo SDK client
-        let expo = new Expo();
-
-
-        // The Expo push notification service accepts batches of notifications so
-        // that you don't need to send 1000 requests to send 1000 notifications. We
-        // recommend you batch your notifications to reduce the number of requests
-        // and to compress them (notifications with similar content will get
-        // compressed).
-        let chunks = expo.chunkPushNotifications(pushNotificationsQueue);
-        let tickets = [];
-
-        // Send the chunks to the Expo push notification service. There are
-        // different strategies you could use. A simple one is to send one chunk at a
-        // time, which nicely spreads the load out over time:
-        for (let chunk of chunks) {
-            try {
-                let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-                console.log(ticketChunk);
-                tickets.push(...ticketChunk);
-                // NOTE: If a ticket contains an error code in ticket.details.error, you
-                // must handle it appropriately. The error codes are listed in the Expo
-                // documentation:
-                // https://docs.expo.io/versions/latest/guides/push-notifications#response-format 
-            } catch (error) {
-                console.error(error);
-            }
-        }
-
-        let receiptIds = [];
-        for (let ticket of tickets) {
-            // NOTE: Not all tickets have IDs; for example, tickets for notifications
-            // that could not be enqueued will have error information and no receipt ID.
-            if (ticket.id) {
-                receiptIds.push(ticket.id);
-            }
-        }
-
-        let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
-        (async () => {
-            // Like sending notifications, there are different strategies you could use
-            // to retrieve batches of receipts from the Expo service.
-            for (let chunk of receiptIdChunks) {
-                try {
-                    let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
-                    console.log(receipts);
-
-                    // The receipts specify whether Apple or Google successfully received the
-                    // notification and information about an error, if one occurred.
-                    for (let receipt of receipts) {
-                        if (receipt.status === 'ok') {
-                            continue;
-                        } else if (receipt.status === 'error') {
-                            console.error(`There was an error sending a notification: ${receipt.message}`);
-                            if (receipt.details && receipt.details.error) {
-                                // The error codes are listed in the Expo documentation:
-                                // https://docs.expo.io/versions/latest/guides/push-notifications#response-format 
-                                // You must handle the errors appropriately.
-                                console.error(`The error code is ${receipt.details.error}`);
-                            }
-                        }
-                    }
-                } catch (error) {
-                    console.error(error);
-                }
-            }
-        })();
+          sendNotification(pushMessage);
     }
 }
