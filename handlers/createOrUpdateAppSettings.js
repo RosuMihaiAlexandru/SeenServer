53e9d6ac041c00b0faa31427b8ab37b738fe9cbe
Rosu Mihai <mihai.rosu@additiv.com>
2019-02-02 23:20:21 +0200

    fixed some bugs on app settings

--- handlers/createOrUpdateAppSettings.js
+++ handlers/createOrUpdateAppSettings.js
@@ -1,27 +1,34 @@
 const bcrypt = require('bcryptjs');
 const Boom = require('boom');
 const JWT = require('jsonwebtoken');
-
+const mongoose = require('mongoose');
 const SettingsAndPreferences = require("../models/SettingsAndPreferences");
 
 module.exports = async function (request, reply) {
-  var loggedInUserId = request.payload.loggedInUserId;
-  var isShowMen = request.payload.isShowMen;
-  var isShowWomen = request.payload.isShowWomen;
-  var ageRange = request.payload.isShowMen;
-  var locationRange = request.payload.locationRange;
+  var loggedInUserId = mongoose.Types.ObjectId(request.payload.loggedInUserId.toString());
+  var isShowMen = (request.payload.isShowMen === "true");
+  var isShowWomen = (request.payload.isShowWomen === "true");
+  var ageRangeStart = parseInt(request.payload.ageRangeStart);
+  var ageRangeStop = parseInt(request.payload.ageRangeStop);
+  var locationRangeStop = parseInt(request.payload.locationRangeStop);
 
-  return SettingsAndPreferences.findOne({ memberId: { loggedInUserId } }).then(settingsAndPreferences => {
+  return SettingsAndPreferences.findOne({ memberId: loggedInUserId }, function (err, settingsAndPreferences) {
+    if (err) {
+      reply(err);
+    }
 
     if (settingsAndPreferences) {
       settingsAndPreferences.isShowMen = isShowMen;
       settingsAndPreferences.isShowWomen = isShowWomen;
-      settingsAndPreferences.ageRange = ageRange;
-      settingsAndPreferences.locationRange = locationRange;
+      settingsAndPreferences.ageRange = [ageRangeStart, ageRangeStop];
+      settingsAndPreferences.locationRange = [0, locationRangeStop];
 
       settingsAndPreferences.save(function (err) {
         if (err) {
           reply(Boom.notFound("Error updating the SettingsAndPreferences"));
+        }
+        else {
+          reply({ "status": "Success" });
         }
       });
 
@@ -29,8 +36,8 @@ module.exports = async function (request, reply) {
       var newSettingsAndPreferences = {
         isShowMen: isShowMen,
         isShowWomen: isShowWomen,
-        ageRange: ageRange,
-        locationRange: locationRange
+        ageRange: [ageRangeStart, ageRangeStop],
+        locationRange: [0, locationRangeStop]
       };
       SettingsAndPreferences.create(newSettingsAndPreferences);
     }
--- handlers/loadSettingsAndPreferences.js
+++ handlers/loadSettingsAndPreferences.js
@@ -1,21 +1,24 @@
-
+const mongoose = require('mongoose');
 const SettingsAndPreferences = require("../models/SettingsAndPreferences");
 
-module.exports = async function(request, reply) {
-  var loggedInUserId = request.params.loggedInUserId.toString();
+module.exports = async function (request, reply) {
+    var loggedInUserId = mongoose.Types.ObjectId(request.params.loggedInUserId.toString());
 
-  return SettingsAndPreferences.findOne({ memberId: { loggedInUserId } }).then(
-    settingsAndPreferences => {
-      if (settingsAndPreferences) {
-        reply({
-          settingsAndPreferences: settingsAndPreferences,
-          status: "Success"
-        });
-      } else {
-        reply({
-          status: "NotFound"
-        });
-      }
+    return SettingsAndPreferences.findOne({ memberId: loggedInUserId }), function (err, settingsAndPreferences) {
+        if (err) {
+            reply(err);
+        }
+
+        if (settingsAndPreferences) {
+            reply({
+                settingsAndPreferences: settingsAndPreferences,
+                status: "Success"
+            });
+        } else {
+            reply({
+                status: "NotFound"
+            });
+        }
     }
-  );
+
 };
