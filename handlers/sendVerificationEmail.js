f7aeb68e0cd237e3a357471a8f1147ae5813b85f
Rosu Mihai <mihai.rosu@additiv.com>
2019-02-24 14:59:03 +0200

    changed verify email link

--- handlers/sendVerificationEmail.js
+++ handlers/sendVerificationEmail.js
@@ -6,7 +6,7 @@ module.exports = async function (request, reply) {
     var html = '<!DOCTYPE html>'+
     '<html><head><title>Please verify your email to secure your acount</title>'+
     '</head><body><div>'+
-    '<a href="https://theseenapp.com/verifyEmail">Verify Email</a>'+
+    '<a href="https://theseenapp.com/confirm-email">Verify Email</a>'+
     '</div></body></html>'
     var sentStatus = await EmailSender.submitWithHtml(body, html, subject, sendTo);
     reply(sentStatus);
