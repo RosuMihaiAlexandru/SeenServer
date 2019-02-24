43cf9b4fe7a918010da1864330e46563742413e2
Rosu Mihai <mihai.rosu@additiv.com>
2019-02-24 17:41:10 +0200

    saved verify email status

--- handlers/sendVerificationEmail.js
+++ handlers/sendVerificationEmail.js
@@ -1,13 +1,37 @@
 var EmailSender = require('../helpers/EmailSender');
+const mongoose = require('mongoose');
+const SettingsAndPreferences = require("../models/SettingsAndPreferences");
+
 module.exports = async function (request, reply) {
     var body = 'Hello from Mihai';
     var sendTo = request.payload.sendTo;
+    var loggedInUserId = mongoose.Types.ObjectId(request.payload.loggedInUserId.toString());
     var subject = "Verify Your Email";
     var html = '<!DOCTYPE html>'+
     '<html><head><title>Please verify your email to secure your acount</title>'+
     '</head><body><div>'+
     '<a href="https://theseenapp.com/confirm-email/"' + Date.now() + '>Confirm email</a>'+
     '</div></body></html>'
-    var sentStatus = await EmailSender.submitWithHtml(body, html, subject, sendTo);
-    reply(sentStatus);
+    var sent = await EmailSender.submitWithHtml(body, html, subject, sendTo);
+    if(sent.status === 'success'){
+        return SettingsAndPreferences.findOne({ memberId: loggedInUserId }, function (err, settingsAndPreferences) {
+            if (err) {
+              reply(err);
+            }
+        
+            if (settingsAndPreferences) {
+                settingsAndPreferences.emailSettings.emailVerificationStatus = 'EmailVerificationSent';
+            }
+
+            settingsAndPreferences.save(function (err) {
+                if (err) {
+                  //reply(Boom.notFound("Error updating the SettingsAndPreferences"));
+                }
+                else {
+                  //reply({ "status": "Success" });
+                }
+              });
+        })
+    }
+    reply(sent);
 };
