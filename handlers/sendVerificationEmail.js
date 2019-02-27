4d64d4f9038346b6f349f0dba33ac4d3f03cf20e
Rosu Mihai <mihai.rosu@additiv.com>
2019-02-28 00:45:17 +0200

    fixed html send verification style

--- handlers/sendVerificationEmail.js
+++ handlers/sendVerificationEmail.js
@@ -9,14 +9,13 @@ module.exports = async function (request, reply) {
     var subject = "Verify Your Email";
     var html = '<!DOCTYPE html>'+
     '<html><head><title>Please verify your email to secure your acount</title>'+
-    '</head><body>' + '<div style="color:#085078;font-family:"Montserrat",Arial,sans-serif;font-size:44px;line-height:48px;text-align:center;font-weight:bold;background-color: white;background-color: white;">' +
+    '</head><body>' + '<div style="padding-bottom:37px;border-bottom:2px solid #e5e5e5"><div style="color:#085078;font-family: montserrat,arial,sans-serif;font-size:34px;line-height:48px;text-align:center;font-weight:bold;background-color:white">' +
 
-    '<span style="color:#085078;text-decoration:none">Confirm Your Email</span></div>'+
-     + '<div >'+
+    '<span style="color:#085078;text-decoration:none;">Verify Your Email</span></div></div>'+
 
-     '<div style="border-radius:30px;padding:15px 30px;color:#ffffff;font-family:"Montserrat",Arial,sans-serif;font-size:14px;line-height:18px;text-align:center;letter-spacing:2px;text-transform:uppercase" bgcolor="#085078"> <a href="https://theseenapp.com/confirm-email/"' + Date.now() +  ' rel="noopener noreferrer" style="color:#ffffff;text-decoration:none" target="_blank"><span style="color:#ffffff;text-decoration:none">CONFIRM EMAIL</span></a> </div>' +
-    '<a href="https://theseenapp.com/confirm-email/"' + Date.now() + '>Confirm email</a>'+
-    '</div>' + '<td class="m_1119671538066256696text-footer-2" style="padding:30px 0;color:#7c7c7c;font-family:"Montserrat",Arial,sans-serif;font-size:11px;line-height:20px;text-align:center"> This email was sent by Seen.<br> 54 Springleaze, Bristol, United Kingdom, BS4 2TT<br> ©2018 Seen Corp LTD. | <a href="theseenapp.com" style="color:#7c7c7c;text-decoration:none" target="_blank"><span style="color:#7c7c7c;text-decoration:none">Privacy Policy</span></a> <br> </td>' + '</body></html>'
+     '<div style="margin-top:37px; border-radius:30px;padding:15px 30px;color:#ffffff;font-family:Montserrat,Arial,sans-serif;font-size:14px;line-height:18px;text-align:center;letter-spacing:2px;text-transform:uppercase; background-color:#085078"> <a href="https://theseenapp.com/confirm-email/"' + Date.now() +  ' rel="noopener noreferrer" style="color:#ffffff;text-decoration:none" target="_blank"><span style="color:#ffffff;text-decoration:none">CONFIRM NOW</span></a> </div>' +
+
+ '<div style="padding:30px 0;color:#7c7c7c;font-family:Montserrat,Arial,sans-serif;font-size:11px;line-height:20px;text-align:center"> This email was sent by Seen.<br> 54 Springleaze, Bristol, United Kingdom, BS4 2TT<br> ©2018 Seen Corp LTD. | <a href="https://theseenapp.com" style="color:#7c7c7c;text-decoration:none" target="_blank"><span style="color:#7c7c7c;text-decoration:none">Privacy Policy</span></a> <br> </div></body></html>'
     var sent = await EmailSender.submitWithHtml(body, html, subject, sendTo);
     if(sent.status === 'success'){
         return SettingsAndPreferences.findOne({ memberId: loggedInUserId }, function (err, settingsAndPreferences) {
