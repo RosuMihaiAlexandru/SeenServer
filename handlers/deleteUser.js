24d5c5c2d4a0b2e79168a2265763a10d6489d016
Rosu Mihai <mihai.rosu@additiv.com>
2019-03-05 00:18:04 +0200

    fixed bug

--- handlers/deleteUser.js
+++ handlers/deleteUser.js
@@ -8,69 +8,65 @@ module.exports = async function (request, reply) {
     var loggedInUserId = request.payload.loggedInUserId;
     var deleteReasonList = JSON.parse(request.payload.deleteReasonList);
 
-    try {
-        await User.findOne({ _id: loggedInUserId }, function (err, user) {
-            if (err) {
-                Logger.logErrorAndWarning(err);
-                reply(err);
-            }
+    await User.findOne({ _id: loggedInUserId }, function (err, user) {
+        if (err) {
+            Logger.logErrorAndWarning(err);
+            reply(err);
+        }
 
-            if (user) {
+        if (user) {
 
-                for (var photoIndex = 0; photoIndex < user.userImages.length; photoIndex++) {
-                    var filePath = '';
+            for (var photoIndex = 0; photoIndex < user.userImages.length; photoIndex++) {
+                var filePath = '';
 
-                    var imageToBeDeleted = user.userImages[photoIndex].media;
+                var imageToBeDeleted = user.userImages[photoIndex].media;
 
-                    var match = imageToBeDeleted.substring(21, imageToBeDeleted.length);
-                    if (match.length > 1) {
-                        filePath = "../../../mnt" + match;
-                    }
-                    else {
-                        // Not found
-                    }
-
-                    user.userImages.splice(photoIndex, 1);
-
-
-                    fs.access(filePath, error => {
-                        if (!error) {
-                            fs.unlink(filePath, function (error) {
-                                if (error) {
-                                    Logger.logErrorAndWarning(error);
-                                    reply({ error: error, status: "failure" });
-                                }
-
-                            });
-                        } else {
-                            Logger.logErrorAndWarning(error);
-                            reply({ error: error, status: "failure" });
-                        }
-                    });
+                var match = imageToBeDeleted.substring(21, imageToBeDeleted.length);
+                if (match.length > 1) {
+                    filePath = "../../../mnt" + match;
+                }
+                else {
+                    // Not found
                 }
 
-                Match.deleteMany({ memberId: loggedInUserId }, function (err) {
-                    if (err) {
-                        Logger.logErrorAndWarning(err);
-                        reply({ status: "failure" });
-                    }
-                })
-            }
-        });
+                user.userImages.splice(photoIndex, 1);
 
-        await User.deleteOne({ _id: loggedInUserId }, function (err) {
+
+                fs.access(filePath, error => {
+                    if (!error) {
+                        fs.unlink(filePath, function (error) {
+                            if (error) {
+                                Logger.logErrorAndWarning(error);
+                                reply({ error: error, status: "failure" });
+                            }
+
+                        });
+                    } else {
+                        Logger.logErrorAndWarning(error);
+                        reply({ error: error, status: "failure" });
+                    }
+                });
+            }
+        }
+    });
+
+    return await User.deleteOne({ _id: loggedInUserId }, function (err) {
+
+        Match.deleteMany({ memberId: loggedInUserId }, function (err) {
             if (err) {
                 Logger.logErrorAndWarning(err);
                 reply({ status: "failure" });
             }
-            else {
-                Logger.logDeleteReason(loggedInUserId, deleteReasonList);
-                reply({ status: "success" });
-            }
         })
-    } catch (error) {
-        Logger.logErrorAndWarning(error);
-        reply({ status: "failure" });
-    }
+
+        if (err) {
+            Logger.logErrorAndWarning(err);
+            reply({ status: "failure" });
+        }
+        else {
+            Logger.logDeleteReason(loggedInUserId, deleteReasonList);
+            reply({ status: "success" });
+        }
+    })
 
 };
