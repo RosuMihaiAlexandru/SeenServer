d1ed9744758527d2434566b030e9a5f5f3161697
Rosu Mihai <mihai.rosu@additiv.com>
2019-03-06 01:17:04 +0200

    fixed bug for log feedback

--- handlers/logFeedback.js
+++ handlers/logFeedback.js
@@ -3,8 +3,8 @@ const Logger = require("../helpers/Logger");
 
 module.exports = async function (request, reply) {
     var loggedInUserId = request.params.loggedInUserId;
-    var likeList = JSON.parse(request.payload.likeList);
-    var dislikeList = JSON.parse(request.payload.dislikeList);
+    var likeList = request.payload.likeList === null || request.payload.likeList === undefined ? [] : JSON.parse(request.payload.likeList);
+    var dislikeList = request.payload.dislikeList === null || request.payload.dislikeList === undefined ? [] : JSON.parse(request.payload.dislikeList);
 
     return Logger.logFeedback(loggedInUserId, likeList, dislikeList);
 };
