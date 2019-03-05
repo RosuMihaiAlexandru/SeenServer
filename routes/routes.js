608af03816b788065fa8992585143210fa433a6f
Rosu Mihai <mihai.rosu@additiv.com>
2019-03-06 01:30:24 +0200

    added route for feedback logging

--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,14 @@ const membersChatRoutes = require("./membersChat.routes");
 const usersRoutes = require("./user.routes");
 const venuesRoutes = require("./venues.routes");
 const settingsAndPreferencesRoutes = require("./settingsAndPreferences.routes");
+const logInfoRoutes = require("./logInfo.routes");
 
 const routes = [].concat(
   membersChatRoutes,
   usersRoutes,
   venuesRoutes,
-  settingsAndPreferencesRoutes
+  settingsAndPreferencesRoutes,
+  logInfoRoutes
 );
 
 module.exports = routes;
