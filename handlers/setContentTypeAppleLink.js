dadba8741532864ced7077b16859f54e4b1e2608
Rosu Mihai <mihai.rosu@additiv.com>
2019-03-17 21:15:46 +0200

    changed naming

--- handlers/setContentTypeAppleLink.js
+++ handlers/setContentTypeAppleLink.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 module.exports = async function (request, reply) {
     try {
-        var aasa = fs.readFileSync('../../../mnt/.wellknown/apple-app-site-association');
+        var aasa = fs.readFileSync('../../../mnt/apple-app-site-association');
         app.get('/apple-app-site-association', function(req, res, next) {
              res.set('Content-Type', 'application/json');
              res.status(200).send(aasa);
