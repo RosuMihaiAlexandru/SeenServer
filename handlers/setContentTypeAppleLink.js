e24beae60847dfd455b0e904282be304527b2921
Rosu Mihai <mihai.rosu@additiv.com>
2019-03-17 17:38:45 +0200

    added error handing

--- handlers/setContentTypeAppleLink.js
+++ handlers/setContentTypeAppleLink.js
@@ -2,9 +2,13 @@
 const fs = require('fs');
 
 module.exports = async function (request, reply) {
-    var aasa = fs.readFileSync('../../../../mnt/.wellknown/apple-app-site-association');
-    app.get('/apple-app-site-association', function(req, res, next) {
-         res.set('Content-Type', 'application/json');
-         res.status(200).send(aasa);
-    });
+    try {
+        var aasa = fs.readFileSync('../../../../mnt/.wellknown/apple-app-site-association');
+        app.get('/apple-app-site-association', function(req, res, next) {
+             res.set('Content-Type', 'application/json');
+             res.status(200).send(aasa);
+        });   
+    } catch (error) {
+        reply(error);
+    }
 }
