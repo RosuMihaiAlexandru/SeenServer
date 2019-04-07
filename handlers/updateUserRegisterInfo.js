6ba1cc68303305a35f296d2b7a1fe599261f112b
Rosu Mihai <mihai.rosu@additiv.com>
2019-04-07 20:43:43 +0300

    added missing function

--- handlers/updateUserRegisterInfo.js
+++ handlers/updateUserRegisterInfo.js
@@ -58,3 +58,16 @@ module.exports = async function (request, reply) {
         }
     })
 }
+
+function getFormattedDate() {
+    var date = new Date();
+    var nowDate =
+      date.getFullYear() +
+      "" +
+      (date.getMonth() + 1) +
+      date.getDate() +
+      date.getHours() +
+      date.getMinutes() +
+      date.getSeconds();
+    return nowDate;
+  }
