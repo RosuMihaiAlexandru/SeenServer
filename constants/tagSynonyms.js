c5d8663592a112c94d84d679b5d84f0a5613ae91
Rosu Mihai <mihai.rosu@additiv.com>
2019-04-20 20:00:12 +0300

    fixed some synonyms issues

--- constants/tagSynonyms.js
+++ constants/tagSynonyms.js
@@ -1,22 +1,9 @@
-module.exports = [
-    { tag: "Club", synonyms: [/Nightclub/i, /Club/i, /Night Club/i] },
-    { tag: "Pub", synonyms: [/Bar/i, /Pub/i, /Gastropub/i] },
-    {
-      tag: "Restaurant",
-      synonyms: [/Restaurant/i, /Steak house/i, /Brasserie/i, /Cake shop/i]
-    },
-    { tag: "Museum", synonyms: [/Museum/i] },
-    {
-      tag: "Sports",
-      synonyms: [
-        /Golf/i,
-        /Sports complex/i,
-        /Gym/i,
-        /Bowling/i,
-        /Stadium/i,
-        /Fitness centre/i,
-        /Cricket/i,
-        /Snooker/i
-      ]
-    }
-  ];
+module.exports =
+[
+	{ tag: "Club", synonyms: [/Nightclub/i, /Club/i, /Night Club/i, /Live music venue/i, /Casino/i, /Events Venue/i, /Concert hall/i, /dance/i, /Nightlife/i, /disco/i, /party/i, /rooftop/i, /boogie/i] },
+	{ tag: "Pub", synonyms: [/Bar/i, /Pub/i, /Gastropub/i, /Bar & grill/i, 	/Social club/i, /Steak house/i, /Wine bar/i, /Live Music Bar/i, /Cider bar/i, /Sports bar/i, /Cocktail bar/i, /Cafe/i, /Brasserie/i, /Coffee/i, /Café/i, /Brewery/i, /Tapas bar/i] },
+	{ tag: "Restaurant", synonyms: [/Restaurant/i, /Steak house/i, /Brasserie/i, /Cake shop/i, /lunch/i, /dinner/i, /supper/i, /tea/i, /Coffee/i, /Café/i, /Cafe/i, /pizza/i, /sizzler/i, /meal/i, /food/i, /eat/i, /hungry/i, /starving/i]},		
+	{ tag: "Museum", synonyms: [/Museum/i, /Art Gallery/i, /Performing arts theatre/i, /Drawing/i, /canvas/i, /print/i, /graffitti/i]},
+	{ tag: "Active", synonyms: [/Sports complex/i, /Gym/i, /Bowling/i, /Stadium/i, /Fitness centre/i, /Cricket/i, /Snooker/i, /Bowling Alley/i, /Sports/i, /Garden centre/i, /Snooker and Pool Club/i, /Cricket Ground/i, /Sports club/i, /Gym/i, /Stadium/i, /Zoo/i, /Cricket/i, /Fitness/i, /Rock climbing gym/i, /Board game/i, /Wildlife/i, /Bowling/i, /City park/i, /Leisure/i, /Point of interest/i, /Recreation/i, /Garden/i, /Safari/i, /golf/i]}
+];
+
