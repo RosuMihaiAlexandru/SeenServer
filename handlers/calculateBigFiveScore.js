ff5d04c66e89ca0f58cec8125a04bbcc540a0fec
Mihai <malexandru.rosu@gmail.com>
2019-08-03 22:56:16 +0300

    updated calculatebigfivescore

--- handlers/calculateBigFiveScore.js
+++ handlers/calculateBigFiveScore.js
@@ -3,8 +3,8 @@ const getResult = require('@alheimsins/b5-result-text')
 
 module.exports = async function (request, reply) {
     try {
-        const { data } = await request.query.data;
-        const scores = calculateScore(data.answers);
+        const { answers } = await request.payload.answers;
+        const scores = calculateScore(answers);
         reply({ data: getResult({ scores, lang: data.lang || 'en' }) });
     } catch (error) {
         throw error
