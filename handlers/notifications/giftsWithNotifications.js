d8eb6d4d65312fed22af0338c58e1a715fcb33b7
Mihai <malexandru.rosu@gmail.com>
2019-08-18 09:56:42 +0300

    modified function

--- handlers/notifications/giftsWithNotifications.js
+++ handlers/notifications/giftsWithNotifications.js
@@ -17,25 +17,36 @@ module.exports = async function (request, reply) {
     var senderAvatar = request.payload.senderAvatar;
 
 
-    User.findOneAndUpdate({ _id: member1Id }, { $inc: { arGiftsLeft: -1 } }, { useFindAndModify: false },function(err, response) {
+    await User.findOne({ _id: member1Id }, function(err, user) {
         if (err) {
-            Logger.logErrorAndWarning(member1Id, err);
-       }
-       var giftMessage = {
-        app_id: "e8d3a93c-398c-407d-9219-8131322767a0",
-        contents: { en: member1Name + " sent you a special AR gift!" },
-        data: {
-            notificationType: "ar-gift",
-            senderId: member1Id,
-            giftKey: giftKey,
-            messageText: messageText,
-            senderName: member1Name,
-            senderAvatar: senderAvatar
-        },
-        include_player_ids: member2PlayerIds
-    };
+          Logger.logErrorAndWarning(member1Id, err);
+        }
+    
+        if (user) {
+          user.arGiftsLeft = user.arGiftsLeft - 1;
+          // user.paymentInfo.receipts.push(receipt);
+          user.save(function(err) {
+            if (err) {
+              Logger.logErrorAndWarning(member1Id, err);
+            }
 
-    NotificationsProcessor.process(giftMessage);
-    reply(match);
-    }) 
+            var giftMessage = {
+                app_id: "e8d3a93c-398c-407d-9219-8131322767a0",
+                contents: { en: member1Name + " sent you a special AR gift!" },
+                data: {
+                    notificationType: "ar-gift",
+                    senderId: member1Id,
+                    giftKey: giftKey,
+                    messageText: messageText,
+                    senderName: member1Name,
+                    senderAvatar: senderAvatar
+                },
+                include_player_ids: member2PlayerIds
+            };
+        
+            NotificationsProcessor.process(giftMessage);
+            reply(match);
+          });
+        }
+      });
 };
