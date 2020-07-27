b4905d15f7266208dafbf43792ad8e5baa318e4c
Florin G. Pantiru <fpantiru@ditech.ro>
2020-07-27 09:48:52 +0300

    adjust check for existing user

--- handlers/checkIfUserExists.js
+++ handlers/checkIfUserExists.js
@@ -1,32 +1,16 @@
 const User = require('../models/User');
-const Logger = require("../helpers/Logger");
-const SettingsAndPreferences = require("../models/SettingsAndPreferences");
-const JWT = require('jsonwebtoken');;
-const config = require('../config');
-const secret = config.jwt.secret;
-const expiresIn = config.jwt.expiresIn;
 
 module.exports = async function (request, reply) {
     const email = request.query.email;
 
-    await User.findOne({ email: email }, function (err, user) {
+    User.findOne({ email: email }, function (err, user) {
         if (err) {
-            Logger.logErrorAndWarning(user._id, err);
             reply({status: 'failure', exists: false});
             return;
         }
 
         if (user) {
-            SettingsAndPreferences.findOne({ memberId: user._id }, function (err, settingsAndPreferences) {
-                if (!err) {
-                  const token = JWT.sign({ email: user.email }, secret, { expiresIn });
-                  reply({ status: 'success', exists: true, token, user: user, appSettings: settingsAndPreferences });
-                }
-                else {
-                  Logger.logErrorAndWarning(user._id, err);
-                  reply({ status: "failure", exists: false });
-                }
-            })           
+            reply({status: 'success', exists: true});         
         } else {
             reply({status: 'success', exists: false});
         }
